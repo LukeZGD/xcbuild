6826cd1e0534559f4c58efa2cb766cb889798e7b
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2025-05-26 13:22:04 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -12,21 +12,26 @@ echo "Setting up build location and permissions"
 sudo chown -R $USER:$USER $DIR
 sudo chown -R $USER:$USER /usr/local
 
+git clone --filter=blob:none https://github.com/madler/zlib
 git clone --filter=blob:none https://github.com/GNOME/libxml2
+
+echo "Building zlib..."
+cd $DIR/zlib
+./configure --static
+make $JNUM install LDFLAGS="-all-static"
+
 echo "Building libxml2..."
-cd libxml2
+cd $DIR/libxml2
 git checkout v2.11.0
 mkdir build
 cd build
-cmake .. -D BUILD_SHARED_LIBS=OFF -D LIBXML2_WITH_LZMA=OFF
+cmake .. -D BUILD_SHARED_LIBS=OFF -D LIBXML2_WITH_LZMA=OFF -D LIBXML2_WITH_ZLIB=OFF
 make $JNUM
 make $JNUM install
 
-cd $DIR
-cd ..
+echo "Building xcbuild..."
+cd $DIR/..
 git submodule update --init --recursive
-export CFLAGS=$(xml2-config --cflags)
-export LIBS=$(xml2-config --libs)
 make
 
 echo "Done!"
